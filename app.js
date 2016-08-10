c6e31adff45c499c2cc6d1b82c969c4ec1c31bf9
SarasArya <saras.arya@gmail.com>
2016-08-10 18:41:14 +0530

    modified app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ let server = require('http').Server(app);
 
 app.use(express.static(path.join(__dirname)));
 
+app.get('/', function(req, res, next){
+	res.sendStatus(200);
+});
+
 app.get('/blog.html', function(req, res,next){
 	res.sendFile(path.join(__dirname+"/blog.html"));
 });
