937912cfb0efbd3f0c1d283ed15486c8d8cb3b06
SarasArya <saras.arya@gmail.com>
2016-08-15 21:56:43 +0530

    reverting back as I found the issue

--- app.js
+++ app.js
@@ -30,8 +30,31 @@ app.post('/contact', function(req, res, next) {
     content = new helper.Content("text/plain", `Somebody with name ${req.body.name} tried to contact you from $req.body.email} with the message ${req.body.message}`);
     mail = new helper.Mail(from_email, subject, to_email, content);
 
-}); 
+    var sg = require('sendgrid')(sendgridAPIKey);
+    var requesBody = mail.toJSON();
+    var request = sg.emptyRequest();
+    request.method = 'POST';
+    request.path = '/v3/mail/send';
+    request.body = requesBody;
+    sg.API(request, function(error, response) {
+        console.log(response.statusCode);
+        if (response.statusCode >= 200 && response.statusCode <= 299)
+            res.sendStatus(200);
+        console.log(response.body);
+    });
+});
+
+app.get('/download', function(req, res, next) {
+    var file = __dirname + '/saras-arya.pdf';
+    res.download(file, 'saras-arya.pdf', function(err) {
+        if (err)
+            throw err;
+        else {
+            console.log("Done download");
+        }
+    });
+});
 var port = process.env.PORT || 8000;
 server.listen(port, function() {
-    console.log("App is running on port "+ port);
+    console.log("App is running on port " + port);
 });
